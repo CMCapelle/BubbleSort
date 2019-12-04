0bd2722387fb8b99b794e0e14e1f152447e74e50
Corey Capelle <ccapelle@student.cscc.edu>
2019-12-03 20:26:01 -0500

    Initial commit

---
+++ src/edu/cscc/BubbleSort.java
@@ -0,0 +1,23 @@
+package edu.cscc;
+
+public class BubbleSort {
+    public static void sort(String[] list) {
+        // implement a Bubble Sort for a String array
+        // Use the integer Bubble Sort from Unit 14 as a starting point
+        // You'll need to use the String compareTo() method to compare Strings
+        boolean changed = true;
+        do {
+            changed = false;
+            for (int j = 0; j <= list.length - 2; j++)
+                if (list[j].compareTo(list[j+1]) > 0) {
+                    //swap list[j] with list[j+1]
+                    String temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    changed = true;
+                }
+        } while (changed);
+    }
+}
+
+
